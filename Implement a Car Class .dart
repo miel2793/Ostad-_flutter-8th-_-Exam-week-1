d0fbc617efc24c0961da23d437de5bef80c78ee3
MD: Miel Mahmud  Sifat <125751571+miel2793@users.noreply.github.com>
2024-09-27 23:38:30 +0600

    Update Implement a Car Class .dart

--- Implement a Car Class .dart
+++ Implement a Car Class .dart
@@ -3,7 +3,7 @@ main()
 Car car=Car('Toyota', ' Corolla',2015);
 
 print('Brand: ${car.brand}');
-print('Modle:${car.modle}');
+print('modle:${car.modle}');
 print('Year:  ${car.year}');
 print('Car Age: ${car.carage()} years');
 }
